52013e7b04c005d9b13e99dc17fd0abdab6dc0ac
Brad Chao <brad@brad.tw>
2018-07-05 11:45:00 +0800

    v2

--- app/src/main/java/tw/brad/brad_lottery/MainActivity.java
+++ app/src/main/java/tw/brad/brad_lottery/MainActivity.java
@@ -3,58 +3,51 @@ package tw.brad.brad_lottery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView[] g1 = new TextView[6];
-    private int[] g1n = {R.id.g1_n1,R.id.g1_n2,R.id.g1_n3,
-            R.id.g1_n4,R.id.g1_n5,R.id.g1_n6};
-    private TextView[] g2 = new TextView[6];
-    private int[] g2n = {R.id.g2_n1,R.id.g2_n2,R.id.g2_n3,
-            R.id.g2_n4,R.id.g2_n5,R.id.g2_n6};
-    private TextView[] g3 = new TextView[6];
-    private int[] g3n = {R.id.g3_n1,R.id.g3_n2,R.id.g3_n3,
-            R.id.g3_n4,R.id.g3_n5,R.id.g3_n6};
+    private ListView list;
+    private SimpleAdapter adapter;
+    private LinkedList<HashMap<String,String>> data;
+    private String[] from = {"title","num0","num1","num2","num3","num4","num5"};
+    private int[] to = {R.id.item_title,R.id.item_num0,R.id.item_num1,R.id.item_num2
+            ,R.id.item_num3,R.id.item_num4,R.id.item_num5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        list = findViewById(R.id.list);
         initView();
     }
 
     private void initView(){
-        for (int i=0; i<g1.length; i++) {
-            g1[i] = findViewById(g1n[i]);
-            g1[i].setText("-");
-        }
-        for (int i=0; i<g2.length; i++) {
-            g2[i] = findViewById(g2n[i]);
-            g2[i].setText("-");
-        }
-        for (int i=0; i<g3.length; i++) {
-            g3[i] = findViewById(g3n[i]);
-            g3[i].setText("-");
-        }
-
+        data = new LinkedList<>();
+        adapter = new SimpleAdapter(this,data,R.layout.item,from,to);
+        list.setAdapter(adapter);
     }
 
     public void lottery(View view) {
-        int[] nums1 = createLottery();
-        for (int i=0; i<g1.length; i++) {
-            g1[i].setText("" + nums1[i]);
-        }
-        int[] nums2 = createLottery();
-        for (int i=0; i<g2.length; i++) {
-            g2[i].setText("" + nums2[i]);
-        }
-        int[] nums3 = createLottery();
-        for (int i=0; i<g3.length; i++) {
-            g3[i].setText("" + nums3[i]);
-        }
+        int[] lottery = createLottery();
+        HashMap<String,String> row = new HashMap<>();
+        row.put(from[0], "第 " + (data.size()+1) + " 組");
+        row.put(from[1], "" + lottery[0]);
+        row.put(from[2], "" + lottery[1]);
+        row.put(from[3], "" + lottery[2]);
+        row.put(from[4], "" + lottery[3]);
+        row.put(from[5], "" + lottery[4]);
+        row.put(from[6], "" + lottery[5]);
+        data.add(row);
+        adapter.notifyDataSetChanged();
+
+        list.smoothScrollToPosition(row.size()-1);
     }
 
     private int[] createLottery(){
